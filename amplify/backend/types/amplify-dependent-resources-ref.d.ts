b22ebaedcbb441ce01d1325ef3fb69c55b03e2f7
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-02-03 16:05:32 -0500

    add google auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
         "amplifyreactbf6a0cfe": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",

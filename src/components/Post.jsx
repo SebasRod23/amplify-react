f5b2795ef0b2cadca4ea6fe32a31cb5c9c4d1078
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-02-04 11:04:24 -0500

    add email alert

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,38 +1,44 @@
-import { Button, Card, Flex, Heading, Text } from '@aws-amplify/ui-react';
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Card, Flex, Heading, Text } from "@aws-amplify/ui-react";
 
 const Post = (props) => {
   return (
-    <Card width='95vw' margin='10px auto' border='1px solid grey'>
+    <Card width="95vw" margin="10px auto" border="1px solid grey">
       <Flex
-        alignContent='center'
-        alignItems='center'
-        justifyContent='space-between'
-        gap='25%'
-        padding='5px 15px'
+        alignContent="center"
+        alignItems="center"
+        justifyContent="space-between"
+        gap="25%"
+        padding="5px 15px"
       >
         <Flex
-          alignItems='center'
-          alignContent='center'
-          justifyContent='left'
-          flex='max-content'
-          width='fit-content'
-          gap='5%'
+          alignItems="center"
+          alignContent="center"
+          justifyContent="left"
+          flex="max-content"
+          width="fit-content"
+          gap="5%"
         >
-          <Heading level={5} fontStyle='bold' minWidth='20%' textAlign='left'>
+          <Heading level={5} fontStyle="bold" minWidth="20%" textAlign="left">
             {props.org.toUpperCase()}
           </Heading>
-          <Text minWidth='20%' textAlign='left'>
+          <Text minWidth="20%" textAlign="left">
             {props.pos}
           </Text>
-          <Text minWidth='20%'>{props.reg.toUpperCase()}</Text>
+          <Text minWidth="20%">{props.reg.toUpperCase()}</Text>
           {props.hasVisaSponsor && (
-            <Text minWidth='20%' textAlign='left' color='#04956e'>
+            <Text minWidth="20%" textAlign="left" color="#04956e">
               Visa Sponsored
             </Text>
           )}
         </Flex>
-        <Button variation='primary' size='small'>
+        <Button
+          variation="primary"
+          size="small"
+          onClick={() => {
+            props.handleClick();
+          }}
+        >
           Connect
         </Button>
       </Flex>
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Predicates, SortDirection } from 'aws-amplify';
-import { Button, Flex, Heading, IconAdd, View } from '@aws-amplify/ui-react';
-import { DataStore } from '@aws-amplify/datastore';
+import React, { useEffect, useState } from "react";
+import { Predicates, SortDirection } from "aws-amplify";
+import {
+  Button,
+  Flex,
+  Heading,
+  IconAdd,
+  View,
+  Alert,
+} from "@aws-amplify/ui-react";
+import { DataStore } from "@aws-amplify/datastore";
 
-import Post from '../components/Post';
-import { Post as iPost } from '../models';
-import FormModal from '../components/FormModal';
+import Post from "../components/Post";
+import { Post as iPost } from "../models";
+import FormModal from "../components/FormModal";
 
 const Dashboard = (props) => {
   const [open, setOpen] = useState(false);
-  const [type, setType] = useState('req' | 'post');
+  const [type, setType] = useState("req" | "post");
   const [posts, setPosts] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleClick = async () => {
+    setShowAlert(true);
+  };
 
   const handleOpen = (type) => {
     setOpen(true);
@@ -30,24 +42,35 @@ const Dashboard = (props) => {
   }, []);
 
   return (
-    <View padding='3%'>
+    <View padding="3%">
+      {showAlert ? (
+        <Alert
+          variation="success"
+          isDismissible={true}
+          onDismiss={() => setShowAlert(false)}
+        >
+          Email Sent!
+        </Alert>
+      ) : (
+        <></>
+      )}
       <Flex
-        alignItems='center'
-        justifyContent='space-between'
-        margin='0 auto 10px'
+        alignItems="center"
+        justifyContent="space-between"
+        margin="20px auto 10px"
       >
         <Heading level={5}>Recently posted:</Heading>
-        <Flex alignItems='center' gap='5%'>
-          <Button size='small' onClick={() => handleOpen('req')}>
+        <Flex alignItems="center" gap="5%">
+          <Button size="small" onClick={() => handleOpen("req")}>
             Request
           </Button>
           <Button
-            variation='primary'
-            size='small'
-            onClick={() => handleOpen('post')}
+            variation="primary"
+            size="small"
+            onClick={() => handleOpen("post")}
           >
-            <Flex alignItems='center' justifyContent='center'>
-              <IconAdd fontSize='1.25rem' />
+            <Flex alignItems="center" justifyContent="center">
+              <IconAdd fontSize="1.25rem" />
               New
             </Flex>
           </Button>
@@ -62,6 +85,7 @@ const Dashboard = (props) => {
             reg={post.region}
             hasVisaSponsor={post.hasVisaSponsor}
             key={post.id}
+            handleClick={handleClick}
           />
         );
       })}
